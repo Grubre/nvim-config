ec3ae1b06027568f9d7b140de5489b9ec5a8c7be
Grubre <ogrodowczykjakub@gmail.com>
2024-10-17 10:08:10 +0200

    Formatted telescope file

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,11 +1,12 @@
 return {
     'nvim-telescope/telescope.nvim', branch = '0.1.x',
+    event = 'UIEnter',
     dependencies = { 'nvim-lua/plenary.nvim', "nvim-telescope/telescope-file-browser.nvim" },
     opts = {
-	pickers = {
-		find_files = {
-			find_command = { "fd", "--type", "f", "--strip-cwd-prefix" },
-		},
-	},
+        pickers = {
+            find_files = {
+                find_command = { "fd", "--type", "f", "--strip-cwd-prefix" },
+            },
+        },
     }
 }
